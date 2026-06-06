423bc9d29caab11996683a8b536650eac4d19388
Mariraja Selvakumar <marirajaselvakumar2000@gmail.com>
2026-06-06 15:38:25 +0530

    add User service on frontend

---
+++ frontend/src/services/userService.ts
@@ -0,0 +1,38 @@
+import apiClient from './apiClient';
+
+export interface User {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  isActive: boolean;
+  lastLoginAt: string | null;
+  permissions?: string[];
+}
+
+export const userService = {
+  getUsers: async (filters: any) => {
+    const { data } = await apiClient.get('/users', { params: filters });
+    return data.data;
+  },
+  getUser: async (id: number) => {
+    const { data } = await apiClient.get(`/users/${id}`);
+    return data.data;
+  },
+  inviteUser: async (userData: any) => {
+    const { data } = await apiClient.post('/users/invite', userData);
+    return data.data;
+  },
+  updateUser: async (id: number, userData: any) => {
+    const { data } = await apiClient.put(`/users/${id}`, userData);
+    return data.data;
+  },
+  updateUserRoles: async (id: number, roleIds: number[]) => {
+    const { data } = await apiClient.put(`/users/${id}/roles`, { roleIds });
+    return data.data;
+  },
+  deleteUser: async (id: number) => {
+    const { data } = await apiClient.delete(`/users/${id}`);
+    return data.data;
+  },
+};
