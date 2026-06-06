d3cb164f9796b442349ba3e53225280ff340ca47
Mariraja Selvakumar <marirajaselvakumar2000@gmail.com>
2026-06-06 18:16:14 +0530

    fix ProtectedRoute authentication check

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -7,7 +7,7 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ requiredPermission }) => {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated());
   const user = useAuthStore((state) => state.user);
   const location = useLocation();
 
